fb6eb3b3c92acf92482d04dc649b6e9472b108de
Fajar Abdurrahman <kamentosa@gmail.com>
2022-11-08 11:00:58 +0700

    create table

---
+++ server/src/config/config_db.js
@@ -0,0 +1,15 @@
+import { createConnection } from 'mysql';
+
+const database = createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'miolica'
+})
+
+database.connect((err) => { 
+    if(err) throw err
+    console.log("mysql is running...");
+})
+
+export default database
