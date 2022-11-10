0d275fef88a6750750a99d136b98b21c156769ae
infinitedim <saputradimas.boediluhur@gmail.com>
2022-11-10 13:36:24 +0700

    conditional rendering

--- server/src/config/config_db.js
+++ server/src/config/config_db.js
@@ -1,15 +1,15 @@
-import { createConnection } from 'mysql';
+import { createConnection } from "mysql";
 
 const database = createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'miolica'
-})
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "miolica",
+});
 
-database.connect((err) => { 
-    if(err) throw err
-    console.log("mysql is running...");
-})
+database.connect((err) => {
+  if (err) throw err;
+  console.log("mysql is running...");
+});
 
-export default database
+export default database;
--- server/src/config/create_table.js
+++ server/src/config/create_table.js
@@ -1,13 +1,13 @@
-import database from './config_db'
+import database from "./config_db";
 
 const sql = `CREATE TABLE dummyimg (
                 id INT NOT NULL AUTO_INCREMENT,
                 text VARCHAR(255),
                 img TEXT,
                 PRIMARY KEY(id)
-            )`
+            )`;
 
-database.query(sql, err => {
-    if( err ) throw err
-    console.log('Table successfully created!!');
-})
+database.query(sql, (err) => {
+  if (err) throw err;
+  console.log("Table successfully created!!");
+});
