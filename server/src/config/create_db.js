75d54c650f156e649a2ee935cb67a52719facc6c
infinitedim <saputradimas.boediluhur@gmail.com>
2022-12-03 09:12:33 +0700

    remove server folder and  dependencies also add cssnano into postcss

--- server/src/config/create_db.js
+++
@@ -1,8 +0,0 @@
-import database from "./config_db";
-
-const sql = "CREATE DATABASE miolica";
-
-database.query(sql, (err) => {
-  if (err) throw err;
-  console.log("Database successfully created!!");
-});
