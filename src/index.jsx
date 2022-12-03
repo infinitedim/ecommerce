1372f434f629a0490767830abd9caae295e01710
infinitedim <saputradimas.boediluhur@gmail.com>
2022-12-03 08:59:08 +0700

    remove workbox and change to vite-plugin-pwa

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { unregister } from "./serviceWorkerRegistration";
-import reportWebVitals from "./reportWebVitals";
 import { store } from "./app/store";
 import App from "./App";
 import ScrollToTop from "./utils/ScrollToTop";
@@ -19,13 +17,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </Provider>
   </React.StrictMode>,
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
